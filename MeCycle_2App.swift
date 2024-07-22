c55862ae924d69b09b93d40da2acde0467705663
fuadfarhan05 <fuadhoquefarhan05@gmail.com>
2024-07-22 14:46:11 -0400

    Add files via upload

---
+++ MeCycle_2App.swift
@@ -0,0 +1,17 @@
+//
+//  MeCycle_2App.swift
+//  MeCycle 2
+//
+//  Created by Fuad Farhan on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MeCycle_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
